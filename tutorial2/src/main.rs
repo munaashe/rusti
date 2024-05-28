30bb93833664f1d463abf724542c2f34fdf70ca2
munaashe <denis.siduna@gmail.com>
2024-05-28 18:27:42 +0200

    tutorial 3

--- tutorial2/src/main.rs
+++ tutorial2/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    const HOURS_IN_A_DAY: i32 = 24;
+    let x = 4;
+    println!("x is: {}", x);
+
+    {
+        let x = x - 2;
+        println!("x is: {}", x);
+    }
+
+    let x = "salibonani";
+    println!("x is: {}", x);
+    println!("{}", HOURS_IN_A_DAY)
 }
